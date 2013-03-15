3422eb1f31a166b42abda347cdf19cc7aa216112
Denis Poltoratsky <denis.poltoratsky@cyberhull.com>
2013-03-16 03:03:44 +0400

    AP-6 - implemented function for conversion of any function arguments to the array of String tags

--- protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
+++ protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
@@ -21,14 +21,45 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     
     
     public function add() {
-        
+        $tagsList = $this->getTagsList( func_get_args() );
     }
     
     
     public function remove() {
-        
+        $tagsList = $this->getTagsList( func_get_args() );        
     }    
     
+    
+    protected function getTagsList( $methodArguments ) {
+        $result = Array();
+        
+        foreach ( $methodArguments as $tagList ) {
+                        
+            if ( !is_array( $tagList ) ) {
+                
+                if ( is_string( $tagList ) ) {
+                    $tagList = explode( ',', $tagList );                    
+                    
+                } else {
+                    $tagList = Array( $tagList );                    
+                }
+            }
+            
+            foreach ( $tagList as $tag ) {
+                
+                if ( is_object( $tag ) && !method_exists( $tag, '__toString' ) ) {
+                    throw new Exception( 'It is unable to typecast to String object of class '.  get_class( $tag ) );                    
+                }
+
+                $tag = (string) $tag;
+                                
+                $result[$tag] = trim( strip_tags( $tag ) );
+            }            
+        }
+        
+        return $result;
+    }
+        
 }
 
 ?>

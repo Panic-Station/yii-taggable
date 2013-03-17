9e31d9924cc8caf7243b94f2bf9a10f3527deca6
Denis Poltoratsky <denis.poltoratsky@cyberhull.com>
2013-03-18 02:06:28 +0400

    AP-8 - code has been documented

--- protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
+++ protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
@@ -2,32 +2,125 @@
 
 
 
+/**
+ * Behaviour for ActiveRecord that allows to attach tags to a model.
+ * 
+ * @author Denis Poltoratsky <356187@gmail.com>
+ * 
+ * @link https://github.com/dpoltoratsky/Taggable
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php The BSD 2-Clause License
+ * 
+ * @package ext.Su_MpaK.Taggable.behaviours
+ * 
+ * @version 1.11.07.01
+ */
 class TaggableBehaviour extends CActiveRecordBehavior {
-    
+   
+    /**
+     * Tag model path alias.
+     * 
+     * Will be passed as 'class' attribute value to Yii::createComponent().
+     * 
+     * @var string 
+     * 
+     * @see YiiBase::createComponent()
+     */
     public $tagModel = null;
                
+    /**
+     * The field name which contains tag title.
+     * 
+     * Will be passed to CActiveRecord::getAttribute().
+     * 
+     * @var string
+     * 
+     * @see CActiveRecord::getAttribute()
+     */
     public $tagTableTitle = 'title';    
     
+    /**
+     * The name of relation table.
+     * 
+     * By default will be '{modelTableName}_{tagTableName}'.
+     * 
+     * @var string
+     */
     public $tagRelationTable = null;    
     
+    /**
+     * The name of attribute in relation table which recalls tag.
+     * 
+     * By default will be '{tagTableName}Id'.
+     * 
+     * @var string
+     */
     public $tagRelationTableTagFk = null;    
     
+    /**
+     * The name of attribute in relation table which recalls model.
+     * 
+     * By default will be '{modelTableName}Id'.
+     * 
+     * @var string
+     */
     public $tagRelationTableModelFk = null;
     
+    /**
+     * Separator for tags in strings.
+     * 
+     * @var string
+     */
     public $tagsSeparator = ',';
     
+    /**
+     * The list of attached to model tags.
+     * 
+     * @var CMap 
+     * 
+     * @see CMap
+     */
     protected $tagsList;
     
+    /**
+     * Instance of blank (without attributes) tag model for internal usage.
+     * 
+     * @var CActiveRecord
+     * 
+     * @see CActiveRecord
+     */
     protected $blankTagModel = null;
     
+    /**
+     * Shows were tags already loaded from DB or not.
+     * 
+     * @var bool
+     */
     protected $tagsAreLoaded = false;
     
     
+    /**
+     * Initialises tagsList with new CMap.
+     * 
+     * @see $tagsList
+     * @see CMap
+     */
     public function __construct() {
         $this->tagsList = new CMap();
     }
     
     
+    /**
+     * Attaches tags to model.
+     * 
+     * Can be called with any number of arguments of any type. Only constraint 
+     * is that Object arguments showld have __toString defined (only exception 
+     * are instances of tag model).
+     * 
+     * @return CActiveRecord Model that behaviour is attached to.
+     * 
+     * @see CActiveRecord
+     */
     public function add() {
         $this->loadTags()->mergeWith( $this->getTagsList( func_get_args() ) );
         
@@ -35,6 +128,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
 
     
+    /**
+     * Removes all attached tags after model has been deleted.
+     * 
+     * @param CModelEvent $event
+     * 
+     * @see CModelEvent
+     */
     public function afterDelete( $event ) {
         $this->clearAttachedTags();            
         
@@ -42,6 +142,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Saves all attached tags.
+     * 
+     * @param CModelEvent $event
+     * 
+     * @see CModelEvent
+     */
     public function afterSave( $event ) {
         $this->loadTags();
         
@@ -72,6 +179,9 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Clears all tags attachments.
+     */
     protected function clearAttachedTags() {
         Yii::app()->db->createCommand()->delete(
             $this->getRelationTable(),
@@ -83,11 +193,26 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Returns all attached to model tags.
+     * 
+     * @param CDbCriteria $additionalCriteria Additional DB criteria to filter attached tags.
+     * @return CMap All attached to the model tags.
+     * 
+     * @see CMap
+     * @see CDbCriteria
+     */
     public function get( $additionalCriteria = null ) {
         return $this->loadTags( $additionalCriteria );        
     }
 
     
+    /**
+     * Returns model table primary key field name.
+     * 
+     * @param bool $full If true - field name will be prepended with model table alias.
+     * @return string Model table primary key field name.
+     */
     protected function getModelPk( $full = true ) {
         $result = $this->owner->tableSchema->primaryKey;
         
@@ -95,10 +220,16 @@ class TaggableBehaviour extends CActiveRecordBehavior {
             $result = $this->owner->tableAlias.'.'.$result;
         }
         
-        return $result;
+        return $result; 
     }
     
     
+    /**
+     * Returns relation table field name that recalls model.
+     * 
+     * @param bool $full If true - field name will be prepended with relational table name.
+     * @return string Relation table field name that recalls model.
+     */
     protected function getRelationModelFk( $full = true ) {
         $result = '';
         
@@ -115,7 +246,12 @@ class TaggableBehaviour extends CActiveRecordBehavior {
         return $result;
     }
     
-        
+    
+    /**
+     * Returns relation table name.
+     * 
+     * @return string Relation table name.
+     */
     protected function getRelationTable() {
         
         if ( empty( $this->tagRelationTable ) ) {           
@@ -129,6 +265,12 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
 
     
+    /**
+     * Returns relation table filed name that recalls tag.
+     * 
+     * @param bool $full If true - field name will be prepended with relation table name.
+     * @return string Relation table filed name that recalls tag.
+     */
     protected function getRelationTagFk( $full = true ) {
         $result = '';
         
@@ -149,7 +291,14 @@ class TaggableBehaviour extends CActiveRecordBehavior {
         return $result;
     }
 
-    
+
+    /**
+     * Returns blank (without attributes set) tag model.
+     * 
+     * @return CActiveRecord Blank (without attributes set) tag model.
+     * 
+     * @see CActiveRecord
+     */
     protected function getTagModel() {
         
         if ( empty( $this->blankTagModel ) ) {            
@@ -164,6 +313,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Returns tag table primary key field name.
+     * 
+     * @param bool $full If true - field name will be prepended with tag table name or alias.
+     * @param bool $alias If true - field will be prepended with table alias, other way - with table name.
+     * @return string Tag table primary key field name.
+     */
     protected function getTagPk( 
         $full = true, 
         $alias = true 
@@ -190,6 +346,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }    
     
     
+    /**
+     * Returns tag table title field name.
+     * 
+     * @param bool $full If true - field name will be prepended with tag table name or alias.
+     * @param bool $alias If true - field will be prepended with table alias, other way - with table name.
+     * @return string Tag table title field name.
+     */
     public function getTagTitle( 
         $full = true, 
         $alias = true 
@@ -198,6 +361,7 @@ class TaggableBehaviour extends CActiveRecordBehavior {
         $result = $this->tagTableTitle;
         
         if ( $full ) {
+            
             /* @var $tagModel CActiveRecord */
             $tagModel = $this->getTagModel();
             
@@ -215,6 +379,15 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Parses array of arguments were passed to one of interface methods and 
+     * creates a has map (CMap) of corresponding tag objects.
+     * 
+     * @param Array $methodArguments The list of input parameters were passed to interface method.
+     * @return CMap List of tag object corresponding to input parameters.
+     * 
+     * @see CMap
+     */
     protected function getTagsList( $methodArguments ) {
         $result = new CMap();
         
@@ -234,6 +407,15 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }         
     
     
+    /**
+     * Checks whether or not specified tags are attached to the model.
+     * 
+     * Can be called with any number of arguments of any type. Only constraint 
+     * is that Object arguments showld have __toString defined (only exception 
+     * are instances of tag model).
+     * 
+     * @return boolean True if ALL specified tags are attached to the model.
+     */
     public function has() {
         $this->loadTags();
         
@@ -253,6 +435,15 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
         
+    /**
+     * Loads all attached to the model tags from the DB.
+     * 
+     * @param CDbCriteria $additionalCriteria
+     * @return CMap All attached to the model tags.
+     * 
+     * @see CMap
+     * @see CDbCriteria
+     */
     protected function loadTags( $additionalCriteria = null ) {
         
         if ( !$this->tagsAreLoaded ) {
@@ -264,10 +455,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
 
             $tagsList = $tagModel->model()->findAll( $criteria );
             
-            $tagTableTitle = $this->getTagTitle( false );
+            $tagTableTitle = $this->getTagTitle( false );            
             
+            /* @var $tag CActiveRecord */
             foreach ( $tagsList as $tag ) {
-                $this->tagsList[$tag->$tagTableTitle] = $tag;                
+                $tagTitle = $tag->getAttribute( $tagTableTitle );
+                
+                $this->tagsList[$tagTitle] = $tag;                
             }  
             
             $this->tagsAreLoaded = true;
@@ -277,6 +471,11 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Makes sure that a list of tags is an array.
+     * 
+     * @param Array $tagList
+     */
     private function normalizeTagList( &$tagList ) {
         
         if ( !is_array( $tagList ) ) {
@@ -291,6 +490,14 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }    
     
     
+    /**
+     * Prepares search criteria to find tags attached to the model.
+     * 
+     * @param CDbCriteria $additionalCriteria Additional criteria to filet attached tags.
+     * @return CDbCriteria Criteria to find tags attached to the model.
+     * 
+     * @see CDbCriteria
+     */
     protected function prepareFindTagsCriteria( $additionalCriteria ) {        
         $result = new CDbCriteria(
             Array(
@@ -321,6 +528,15 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Makes sure that tag object will be instance of tag model.
+     * 
+     * @param mixed $tag Initial value of tag
+     * @param string $tagTitle Tag title
+     * @return CActiveRecord Tag object
+     * 
+     * @see CActiveRecord
+     */
     protected function prepareTagObject( 
         $tag, 
         $tagTitle 
@@ -362,6 +578,14 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Prepares string tag title.
+     * 
+     * @param mixed $tag initial tag value
+     * @return string Tag title
+     * 
+     * @throws Exception
+     */
     protected function prepareTagTitle( $tag ) {    
         
         /* @var $tagModel CActiveRecord */        
@@ -369,9 +593,7 @@ class TaggableBehaviour extends CActiveRecordBehavior {
         $tagModelClass = get_class( $tagModel );
         
         if ( $tag instanceof $tagModelClass ) {
-            $tagTableTitle = $this->getTagTitle( false );
-
-            $tagTitle = $tag->$tagTableTitle;                        
+            $tagTitle = $tag->getAttribute( $this->getTagTitle( false ) );                        
 
         } elseif ( is_object( $tag ) && !method_exists( $tag, '__toString' ) ) {             
             throw new Exception( 
@@ -391,6 +613,17 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
    
     
+    /**
+     * Detashes specified tags from the model.
+     * 
+     * Can be called with any number of arguments of any type. Only constraint 
+     * is that Object arguments showld have __toString defined (only exception 
+     * are instances of tag model).
+     * 
+     * @return CActiveRecord Model that behaviour is attached to.
+     * 
+     * @see CActiveRecord
+     */
     public function remove() {
         $this->loadTags();
 
@@ -404,6 +637,13 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }    
     
 
+    /**
+     * Detaches all tags from the model.
+     * 
+     * @return CActiveRecord Model that behaviour is attached to.
+     * 
+     * @see CActiveRecord
+     */
     public function reset() {
         $this->tagsList->clear();
         
@@ -411,13 +651,29 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
     
     
+    /**
+     * Attaches to the model specified set of tags that are replacing all 
+     * previous ones.
+     * 
+     * @return CActiveRecord Model that behaviour is attached to.
+     * 
+     * @see CActiveRecord
+     */
     public function set() {
         $this->tagsList = $this->getTagsList( func_get_args() );        
         
         return $this->owner;        
     }
     
-        
+
+    /**
+     * Modifies the model DB criteria in order to find models with any of 
+     * specidied tags attached.
+     * 
+     * @return CActiveRecord Model that behaviour is attached to.
+     * 
+     * @see CActiveRecord
+     */
     public function taggedWith() {
         $tagsList = $this->getTagsList( func_get_args() );
        
@@ -451,6 +707,14 @@ class TaggableBehaviour extends CActiveRecordBehavior {
     }
         
     
+    /**
+     * Allows all attached to the model tags to be printed imploded by 
+     * separator.
+     * 
+     * @return string Imploded by separator tags attached to the model.
+     * 
+     * @see $tagsSeparator
+     */
     public function __toString() {
         $this->loadTags();
         

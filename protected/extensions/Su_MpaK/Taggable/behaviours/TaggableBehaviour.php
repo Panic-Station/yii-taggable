3c3844ba3c762c1878eefd5549c2df35120d3266
Denis Poltoratsky <denis.poltoratsky@cyberhull.com>
2013-03-17 19:38:14 +0400

    AP-6 - implemented function for loading attached to model tags - implemented "add" function to attach to model specified tags - implemented "set" function to attach to model specified tags with overwriting all previous attached tags - implemented "remove" function to detach from model specified tags - implemented "reset" function to detach from model all previously attached tags

--- protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
+++ protected/extensions/Su_MpaK/Taggable/behaviours/TaggableBehaviour.php
@@ -4,29 +4,126 @@
 
 class TaggableBehaviour extends CActiveRecordBehavior {
     
-    
-    public $tagTable = 'tag';
-        
-    public $tagTablePk = 'id';
-       
+    public $tagModel = null;
+               
     public $tagTableTitle = 'title';    
-    
-    public $tagTableQuantity = 'quantity';    
     
     public $tagRelationTable = null;    
     
-    public $tagRelationTableTagFk = 'tagId';    
+    public $tagRelationTableTagFk = null;    
     
     public $tagRelationTableModelFk = null;
+    
+    protected $tagsList = Array();
+    
+    protected $originalTagsList = Array();
+    
+    protected $blankTagModel = null;
     
     
     public function add() {
         $tagsList = $this->getTagsList( func_get_args() );
+        
+        $this->tagsList = array_unique( array_merge( $this->tagsList, $tagsList ) );        
+    }
+
+    
+    public function afterDelete( $event ) {
+        
+        parent::afterDelete( $event );
     }
     
     
-    public function remove() {
-        $tagsList = $this->getTagsList( func_get_args() );        
+    public function afterSave( $event ) {
+                
+        parent::beforeSave($event);
+    }
+    
+    
+    public function get() {
+        $this->loadTags();
+        
+        // TODO return the list of tags
+    }
+    
+    
+    protected function getRelationModelFk( $full = true ) {
+        $result = '';
+        
+        if ( empty( $this->tagRelationTableModelFk ) ) {
+            $this->tagRelationTableModelFk = $this->owner->tableName().'Id';
+        }
+        
+        if ( $full ) {
+            $result = $this->getRelationTable().'.';
+        }
+        
+        $result .= $this->tagRelationTableModelFk;
+        
+        return $result;
+    }
+    
+        
+    protected function getRelationTable() {
+        
+        if ( empty( $this->tagRelationTable ) ) {           
+            $this->tagRelationTable = 
+                $this->owner->tableName()
+                .'_'
+                .$this->getTagModel()->tableName();
+        }
+        
+        return $this->tagRelationTable;
+    }
+
+    
+    protected function getRelationTagFk( $full = true ) {
+        $result = '';
+        
+        if ( empty( $this->tagRelationTableTagFk ) ) {
+            
+            /* @var $tagModel CActiveRecord */
+            $tagModel = $this->getTagModel();
+            
+            $this->tagRelationTableTagFk = $tagModel->tableName().'Id';
+        }
+        
+        if ( $full ) {                        
+            $result = $this->getRelationTable().'.';
+        }
+        
+        $result .= $this->tagRelationTableTagFk;
+        
+        return $result;
+    }
+
+    
+    protected function getTagModel() {
+        
+        if ( empty( $this->blankTagModel ) ) {            
+            $this->blankTagModel = Yii::createComponent(
+                Array(
+                    'class' => $this->tagModel
+                )
+            );
+        }
+        
+        return $this->blankTagModel;
+    }
+    
+    
+    protected function getTagPk( $full = true ) {
+        
+        /* @var $tagModel CActiveRecord */
+        $tagModel = $this->getTagModel();
+        
+        $result = $tagModel->tableSchema->primaryKey;
+        
+        if ( $full ) {
+            $result .= $tagModel->tableAlias.'.';
+        }
+        
+        return $result;
     }    
     
     
@@ -47,8 +144,15 @@ class TaggableBehaviour extends CActiveRecordBehavior {
             
             foreach ( $tagList as $tag ) {
                 
-                if ( is_object( $tag ) && !method_exists( $tag, '__toString' ) ) {
-                    throw new Exception( 'It is unable to typecast to String object of class '.  get_class( $tag ) );                    
+                if ( 
+                    is_object( $tag ) 
+                    && !method_exists( $tag, '__toString' ) 
+                ) {
+                    
+                    throw new Exception( 
+                        'It is unable to typecast to String object of class '
+                        .get_class( $tag ) 
+                    );                    
                 }
 
                 $tag = (string) $tag;
@@ -58,6 +162,54 @@ class TaggableBehaviour extends CActiveRecordBehavior {
         }
         
         return $result;
+    }
+    
+    
+    protected function loadTags() {
+        
+        /* @var $tagModel CActiveRecord */
+        $tagModel = $this->getTagModel();
+        
+        $relationTable = $this->getRelationTable();
+        $relationTagFk = $this->getRelationTagFk();
+        $relationModelFk = $this->getRelationModelFk();
+        
+        $criteria = new CDbCriteria(
+            Array(
+                
+                'join' => 'INNER JOIN '.$relationTable
+                    .' ON '.$relationTagFk.' = '.$this->getTagPk(),
+                
+                'condition' => $relationModelFk.' = :modelId',
+                
+                'params' => Array(
+                    'modelId' => $this->owner->primaryKey
+                )
+            )
+        );
+        
+        $tagsList = $tagModel->model()->findAll( $criteria );
+        
+        $this->originalTagsList = $tagsList;
+    }
+    
+    
+    public function remove() {
+        $tagsList = $this->getTagsList( func_get_args() );        
+        
+        $this->tagsList = array_diff( $this->tagsList, $tagsList );
+    }    
+    
+
+    public function reset() {
+        $this->tagsList = Array();
+    }
+    
+    
+    public function set() {
+        $tagsList = $this->getTagsList( func_get_args() );        
+
+        $this->tagsList = $tagsList;
     }
         
 }
